0e8fb9f147b1eedc9160a60ac70ee95b434a6b78
独一无二 <565536277@qq.com>
2019-07-26 10:50:55 +0800

    disanci

--- mysite/apps/route_base/urls.py
+++ mysite/apps/route_base/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     url(r'^index/$',views.index,name='index'),
 #url(正则，视图，kws传递给视图的参数，name：为URL视图命名)
     #route_base
+    url(r'zoos/(\d+)/', views.zoos ,name='zoos1'),
+    url(r'zoos/(?P<id>\d+)/', views.zoos2, name='zoos2'),
+    url(r'zoos/(?P<id>\d+)/', views.zoos3,{"type":"dog"} ,name='zoos3'),
+    url(r'^login/$',views.login,{"status":10},name='login')
 
 ]
